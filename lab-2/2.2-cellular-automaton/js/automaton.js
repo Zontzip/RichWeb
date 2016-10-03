383c0ca6f4cece44c5ba2beaa8fe77917ff4d23b
Zontzip <zontzor@gmail.com>
2016-10-03 23:03:10 +0100

    Finish lab-2 part 2 automaton

--- lab-2/2.2-cellular-automaton/js/automaton.js
+++ lab-2/2.2-cellular-automaton/js/automaton.js
@@ -1,61 +1,72 @@
 // Create container
-var container = document.createElement("div");
+let container = document.createElement("div");
 container.id = "container";
 container.style.width = "1010px";
 document.body.appendChild(container);
 
 // Create first row 
-var row = document.createElement("div");
+let row = document.createElement("div");
 row.id = "row1";
 container.appendChild(row);
 
 // Create initial nodes
 for(let i = 0; i < 101; i++) {
-	var state = parseInt(Math.random() * 2) ?  true : false;
-	var cell = createCell(state);
+	let state = parseInt(Math.random() * 2) ?  true : false;
+	let cell = createCell(state);
 	cell.className = "cell-row1";
 	row.appendChild(cell);
 }
 
-for (let i = 2; i < 52; i++) {
+for (let i = 2; i < 51; i++) {
 	// Create current row div
-	var newRow = document.createElement("div");
-	var currentRowStr = i;
+	let newRow = document.createElement("div");
+	let currentRowStr = i;
 	newRowStr = "row" + currentRowStr.toString();
 	newRow.id = newRowStr;
 
 	// Fetch previous row and add new row as child
-	var lastRow = i-1;
+	let lastRow = i-1;
 	parentRowStr = "row" + lastRow.toString();
-	var parentRow = document.getElementById(parentRowStr);
+	let parentRow = document.getElementById(parentRowStr);
 	container.appendChild(newRow);
 
+	// Fetch array of nodes from previous row
 	parentColsStr = "cell-row" + lastRow.toString(); 
 	var parent = document.getElementsByClassName(parentColsStr);
 
 	for (let j = 0; j < 101; j++) {
+		// Check if column is first in row
 		if (j === 0) {
-			var cell = createCell(true);
-		} else if (j === 100) {
-			var cell = createCell(true);
-		} else {
-			if (parent[j].previousSibling.title === "active" && parent[j].title === "active" && parent[j].nextSibling.title === "active") {
-				var cell = createCell(true);
-			} else if (parent[j].previousSibling.title === "active" && parent[j].title === "active" && parent[j].nextSibling.title === "inactive") {
+			if (parent[parent.length-1].title === "active" && parent[j].title === "active" && parent[j].nextSibling.title === "inactive") {
 				var cell = createCell(false);
-			} else if (parent[j].previousSibling.title === "active" && parent[j].title === "inactive" && parent[j].nextSibling.title === "active") {
+			} else if (parent[parent.length-1].title === "inactive" && parent[j].title === "active" && parent[j].nextSibling.title === "active") {
+				var cell = createCell(false);
+			} else if (parent[parent.length-1].title === "inactive" && parent[j].title === "inactive" && parent[j].nextSibling.title === "inactive") {
+				var cell = createCell(false);
+			} else {
 				var cell = createCell(true);
-			} else if (parent[j].previousSibling.title === "active" && parent[j].title === "inactive" && parent[j].nextSibling.title === "inactive") {
+			}
+		// Check if column is last in row
+		} else if (j === 100) {
+			if (parent[j].previousSibling.title === "active" && parent[j].title === "active" && parent[0].title === "inactive") {
+				var cell = createCell(false);
+			} else if (parent[j].previousSibling.title === "inactive" && parent[j].title === "active" && parent[0].title === "active") {
+				var cell = createCell(false);
+			} else if (parent[j].previousSibling.title === "inactive" && parent[j].title === "inactive" && parent[0].title === "inactive") {
+				var cell = createCell(false);
+			} else {
 				var cell = createCell(true);
+			}
+		} else {
+			if (parent[j].previousSibling.title === "active" && parent[j].title === "active" && parent[j].nextSibling.title === "inactive") {
+				var cell = createCell(false);
 			} else if (parent[j].previousSibling.title === "inactive" && parent[j].title === "active" && parent[j].nextSibling.title === "active") {
 				var cell = createCell(false);
-			} else if (parent[j].previousSibling.title === "inactive" && parent[j].title === "active" && parent[j].nextSibling.title === "inactive") {
-				var cell = createCell(true);
-			} else if (parent[j].previousSibling.title === "inactive" && parent[j].title === "inactive" && parent[j].nextSibling.title === "active") {
-				var cell = createCell(true);
 			} else if (parent[j].previousSibling.title === "inactive" && parent[j].title === "inactive" && parent[j].nextSibling.title === "inactive") {
 				var cell = createCell(false);
-			} 
+			} else {
+				var cell = createCell(true);
+			}
 		}
 		cellRowStr = "cell-" + newRowStr;
 		cell.className = cellRowStr;
