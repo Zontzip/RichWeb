a7adf9ebe8f783766edc58932d59df79c774023a
Zontzip <zontzor@gmail.com>
2016-10-13 23:32:10 +0100

    Finish lab 3 part 1 - calc backend

--- lab-3/3.1-calculator-back-end/js/calc.js
+++ lab-3/3.1-calculator-back-end/js/calc.js
@@ -1,56 +1,39 @@
 var buttons = document.querySelectorAll(".flex-item");
 var screen = document.querySelector("input[type=text]");
-val1 = null;
-val2 = null;
 
 var characters = ["(", ")", "±", "÷", "7", 
                   "8", "9", "x", "4", "5", 
                   "6", "-", "1", "2", "3", 
                   "+", "0", ".", "C", "="];
 
+function Compute(btnValue) {
+  var screenValue = screen.value;
+
+  if(btnValue == "C") {
+      screen.value = '';
+    } 
+    else if(btnValue == "=") {
+      var equation = screenValue;
+
+      if (equation.includes("x")) {
+        equation = equation.replace(/x/g, "*");
+      }
+
+      if (equation.includes("÷")) {
+        equation = equation.replace(/÷/g, "/");
+      }
+
+      screen.value = eval(equation);
+    }
+    else {
+      screen.value += btnValue;
+    }
+}
+
 // Add on click events
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].onclick = function(e) {
-    input = this.innerHTML;
-
-    if (input === "C") {
-      screen.value = "";
-      val1 = null;
-      val2 = null;
-      operator = "";
-    } else if (input > -1 && input < 10 || input === ".") {
-      if (val1 === null) {
-        val1 = input;
-      } else {
-        val1 += input;
-      }
-      screen.value = val1;
-    } else if (input === '=') {
-      switch (operator) {
-        case '+':
-          val2 = parseFloat(val2) + parseFloat(val1);
-          screen.value = val2;
-          break;
-        case '-':
-          val2 = parseFloat(val2) - parseFloat(val1);
-          screen.value = val2;
-          break;
-        case 'x':
-          val2 = parseFloat(val2) * parseFloat(val1);
-          screen.value = val2;
-          break;
-        case '÷':
-          val2 = parseFloat(val2) / parseFloat(val1);
-          screen.value = val2;
-          break;
-      }
-    } else {
-      if (val2 === null) {
-        val2 = val1;
-      }
-      operator = input;
-      val1 = "";
-    }
+    Compute(this.innerHTML);
   }
 }
 
@@ -58,17 +41,6 @@ document.onkeypress = function(e) {
   var input = String.fromCharCode(e.keyCode);
 
   if (characters.indexOf(input) > -1) {
-    screen.value = input;
+    Compute(input);
   }
 }
-
-
-
-
-
-
-
-
-
-
-
