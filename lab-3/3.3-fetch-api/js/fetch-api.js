7603775dd4cd3248e809470b98c3115d5480b687
Alex Kiernan <zontzor@gmail.com>
2016-10-24 12:29:23 +0100

    Add lab 3 fetch base code

---
+++ lab-3/3.3-fetch-api/js/fetch-api.js
@@ -0,0 +1,7 @@
+root = "http://jsonplaceholder.typicode.com/"
+userpath = "users";
+
+// A promise is returned
+const whenCatLoaded = fetch('cat.jpg', options);
+ 
+// Use XMLHttpRequest with promises!!!
