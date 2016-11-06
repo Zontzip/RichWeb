d7bdf348d88997fc0a1919477b23cf410757f8a8
Zontzip <zontzor@gmail.com>
2016-11-06 19:55:39 +0000

    Add github api support for pulling profile info

---
+++ lab-4/js/git.js
@@ -0,0 +1,83 @@
+function fetch(url, options) {
+  return new Promise(function(resolve, reject) {
+
+  	// Get the http method before opening connection
+    var httpMethod = '';
+    if ('method' in options) {
+      httpMethod = options.method;
+    } else {
+      httpMethod = 'GET';
+    }
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open(options.method, url);
+    
+    if ('headers' in options) {
+      options.headers.forEach(function(header) {
+        xhr.setRequestHeader(header.name, header.value);
+      });
+    }
+
+    xhr.onload = function() {
+      if (xhr.status == 200) {
+        resolve(xhr.response);
+      } else {
+        reject(Error(xhr.statusText));
+      }
+    };
+
+    xhr.onreadystatechange = function(e) {
+      if (this.readyState === 4 && this.status === 200) {
+        try {
+          console.log('good');
+        }
+        catch (error) {
+          alert("There has been an error");
+          console.log(error);
+          return false;
+        }
+      }
+      else if (this.readyState === 4 && this.status !== 200) {
+         console.log('There has been an error');
+      }
+    };
+
+    // Returns HTTP status, Default OK, 
+    xhr.onerror = function() {
+      reject(Error(xhr.statusText));
+    };
+
+    // Send request
+    xhr.send();
+  });
+}
+
+const root = "https://api.github.com/users/";
+const testUser = "zontzip";
+
+var options = {
+  method : 'GET',
+  headers:[
+    {name: "Content-Type", value: "application/json"}
+  ]
+ };
+
+
+var searchButton = document.querySelector("button[type=submit]");
+
+searchButton.onclick = function(e) {
+  let input = document.querySelector("input[type=text]").value;
+
+  fetch(root + input, options).then(function(data) {
+    var json = JSON.parse(data);
+    document.getElementById("profile-image").src = json.avatar_url;
+    document.getElementById("li-name").innerHTML = json.name;
+    document.getElementById("li-username").innerHTML = json.login;
+    document.getElementById("li-email").innerHTML = json.email;
+    document.getElementById("li-location").innerHTML = json.location;
+    document.getElementById("li-no-of-gists").innerHTML = json.public_gists;
+  }, function(err) {
+    //console.log(err);
+    console.log("The request has failed!");
+  });
+}
