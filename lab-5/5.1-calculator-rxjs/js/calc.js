6664a53b1785c4da1bec9d42695fce946308892d
Zontzip <zontzor@gmail.com>
2016-11-18 21:38:05 +0000

    Add lab 5 core files - copied calc and built package and webpack config

---
+++ lab-5/5.1-calculator-rxjs/js/calc.js
@@ -0,0 +1,50 @@
+var buttons = document.querySelectorAll(".flex-item");
+var screen = document.querySelector("input[type=text]");
+
+const characters = ["(", ")", "±", "÷", "7", 
+                  "8", "9", "x", "4", "5", 
+                  "6", "-", "1", "2", "3", 
+                  "+", "0", ".", "C", "="];
+
+function Compute(btnValue) {
+  var screenValue = screen.value;
+
+  try {
+    if(btnValue == "C") {
+        screen.value = '';
+      } 
+    else if(btnValue == "=") {
+      var equation = screenValue;
+
+      if (equation.includes("x")) {
+        equation = equation.replace(/x/g, "*");
+      }
+
+      if (equation.includes("÷")) {
+        equation = equation.replace(/÷/g, "/");
+      }
+
+      screen.value = eval(equation);
+    }
+    else {
+      screen.value += btnValue;
+    }
+  } catch(err) {
+    console.log("Error occured in the calculation: " + err);
+  }
+}
+
+// Add on click events
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].onclick = function(e) {
+    Compute(this.innerHTML);
+  }
+}
+
+document.onkeypress = function(e) {
+  var input = String.fromCharCode(e.keyCode);
+
+  if (characters.indexOf(input) > -1) {
+    Compute(input);
+  }
+}
