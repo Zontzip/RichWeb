b14616ec989d53ecbad28cb7c838b588dec698aa
Zontzip <zontzor@gmail.com>
2016-11-19 12:16:38 +0000

    Finish calculator functionality, TODO: add +- support

--- lab-5/5.1-calculator-rxjs/index.js
+++ lab-5/5.1-calculator-rxjs/index.js
@@ -1,18 +1,51 @@
 import {Observable} from 'rxjs/Rx';
 
+/**
+ * Get key clicks.
+ */
 const buttons = document.querySelectorAll(".flex-item");
+const expresion$ = Observable.merge (
+  Observable.fromEvent(buttons, 'click')
+    .map(e => e.target.innerHTML),
+  Observable.fromEvent(document, 'keyup')
+    .map(e => e.key)
+)
+
 const display = document.querySelector("input[type=text]");
+const out$ = expresion$
+  .scan((prev, curr) => {
+    if (curr === 'C') {
+      for(var i = 0; i < buttons.length; i++)
+          buttons[i].disabled = false;
+      return '';
+    }
+    else if (curr === '=') {
+      try {
+        return eval(prev);
+      } catch (err) {
+        return 'Error';
+      }
+    }
+    else {
+      if (curr === 'x')
+        curr = curr.replace(/x/g, "*");
+      else if (curr === '÷')
+        curr = curr.replace(/÷/g, "/");
+      return prev+curr;
+    }
+  });
 
-let counter = {value: 0};
-
- const userInput$ = Observable.merge (
-   Observable.fromEvent(buttons, 'click'),
-   Observable.fromEvent(document, 'keyup')
- )
-
-userInput$.scan((acc) => {
-    return {value: acc.value + 1}
-  }, counter)
-  .subscribe(counter => {
-    display.value = counter.value;
-});
+out$.subscribe(function(x) {
+  if (x === 'Error') {
+    for(var i = 0; i < buttons.length; i++) {
+      if (buttons[i].innerHTML !== 'C')
+        buttons[i].disabled = true;
+    }
+    console.log('Error has occured');
+  }
+  display.value = x;
+  },
+  function (err) {
+    console.log('Error: ' + err);
+  }
+);
