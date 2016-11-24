ddae9547325a6b1efe20888123f0d62d5d0fe9a3
Zontzip <zontzor@gmail.com>
2016-11-24 01:02:56 +0000

    Add split,TODO: canvas

--- lab-5/5.2-stopwatch-rxjs/index.js
+++ lab-5/5.2-stopwatch-rxjs/index.js
@@ -6,11 +6,15 @@ const startBtn = document.getElementById("start");
 const stopBtn = document.getElementById("stop");
 const splitBtn = document.getElementById("split");
 const resetBtn = document.getElementById("reset");
+const splitList = document.getElementById("ul-splits");
 
 var counter = {state: 'INACTIVE', m: 0, s: 0, ms: 0};
 
+function counterStr() {
+  return (counter.m + ':' + counter.s + ':' + counter.ms);
+}
+
 function start() {
-  console.log('Start');
   counter.state = 'ACTIVE';
 }
 const startBtn$ = Observable.fromEvent(startBtn, 'click');
@@ -23,7 +27,9 @@ const stopBtn$ = Observable.fromEvent(stopBtn, 'click');
 stopBtn$.map(stop).subscribe();
 
 function split() {
-
+  let newRepoItem = document.createElement("li");
+  newRepoItem.innerHTML = counterStr();
+  splitList.appendChild(newRepoItem);
 }
 const splitBtn$ = Observable.fromEvent(splitBtn, 'click');
 splitBtn$.map(split).subscribe();
@@ -33,18 +39,25 @@ function reset() {
   counter. m = 0;
   counter.s = 0;
   counter.ms = 0;
+
+  var firstChild = splitList.firstChild;
+
+  while (firstChild) {
+    splitList.removeChild(firstChild);
+    firstChild = splitList.firstChild;
+  }
 }
 const resetBtn$ = Observable.fromEvent(resetBtn, 'click');
 resetBtn$.map(reset).subscribe();
 
-const timer$ = Observable.interval(100)
+const timer$ = Observable.interval(10)
   .map(
     function incr() {
       console.log(counter.state);
 
       switch(counter.state) {
         case 'ACTIVE':
-          counter.ms += 10;
+          counter.ms++;
 
           if(counter.ms >= 100) {
             counter.ms = 0;
@@ -55,11 +68,10 @@ const timer$ = Observable.interval(100)
             counter.s = 0;
             counter.m++;
           }
-        case 'INACTIVE':
       }
     }
   );
 
 timer$.subscribe(function() {
-  display.value = (counter.m + ':' + counter.s + ':' + counter.ms);
+  display.value = counterStr();
 });
