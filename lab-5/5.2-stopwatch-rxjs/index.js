954ca581a338f6bb86031ae5ac5d39182a175b43
Zontzip <zontzor@gmail.com>
2016-11-24 00:16:56 +0000

    Finish refactor implementation of counter, TODO: split and canvas

--- lab-5/5.2-stopwatch-rxjs/index.js
+++ lab-5/5.2-stopwatch-rxjs/index.js
@@ -7,45 +7,59 @@ const stopBtn = document.getElementById("stop");
 const splitBtn = document.getElementById("split");
 const resetBtn = document.getElementById("reset");
 
-var counter = {m: 0, s: 0, ms: 0, state: false};
+var counter = {state: 'INACTIVE', m: 0, s: 0, ms: 0};
 
 function start() {
   console.log('Start');
-  counter.state = true;
+  counter.state = 'ACTIVE';
 }
-
-const buttons$ = Observable.merge(
-  Observable.fromEvent(startBtn, 'click').mapTo(start),
-  Observable.fromEvent(stopBtn, 'click').mapTo(start),
-  Observable.fromEvent(splitBtn, 'click').mapTo(start),
-  Observable.fromEvent(resetBtn, 'click').mapTo(start)
-)
-
 const startBtn$ = Observable.fromEvent(startBtn, 'click');
-
 startBtn$.map(start).subscribe();
 
-const timer$ = Observable.interval(1)
+function stop() {
+  counter.state = 'INACTIVE';
+}
+const stopBtn$ = Observable.fromEvent(stopBtn, 'click');
+stopBtn$.map(stop).subscribe();
+
+function split() {
+
+}
+const splitBtn$ = Observable.fromEvent(splitBtn, 'click');
+splitBtn$.map(split).subscribe();
+
+function reset() {
+  counter.state = 'INACTIVE';
+  counter. m = 0;
+  counter.s = 0;
+  counter.ms = 0;
+}
+const resetBtn$ = Observable.fromEvent(resetBtn, 'click');
+resetBtn$.map(reset).subscribe();
+
+const timer$ = Observable.interval(100)
   .map(
     function incr() {
       console.log(counter.state);
-      if (counter.state === true)
-        counter.ms++;
 
-        if(counter.ms >= 1000) {
-          counter.ms = 0;
-          counter.s++;
-        }
+      switch(counter.state) {
+        case 'ACTIVE':
+          counter.ms += 10;
 
-        if(counter.s >= 60) {
-          counter.s = 0;
-          counter.m++;
-        }
+          if(counter.ms >= 100) {
+            counter.ms = 0;
+            counter.s++;
+          }
+
+          if(counter.s >= 60) {
+            counter.s = 0;
+            counter.m++;
+          }
+        case 'INACTIVE':
+      }
     }
   );
 
-timer$.subscribe(
-  x => {
-    console.log(counter.m + ':' + counter.s + ':' + counter.ms);
-  }
-);
+timer$.subscribe(function() {
+  display.value = (counter.m + ':' + counter.s + ':' + counter.ms);
+});
